400e9891bd9fc8ade717155a4536f4e2c1b63149
Vation Code <abdullah@smainsankamil.sch.id>
2022-07-17 06:55:10 +0700

    add profile image

--- src/layouts/Home/Profile.js
+++ src/layouts/Home/Profile.js
@@ -1,7 +1,7 @@
 import profileKatakana from 'assets/katakana-profile.svg?url';
-import profileImgLarge from 'assets/profile-larg.jpg';
+import profileImgLarge from 'assets/profil.png';
 import profileImgPlaceholder from 'assets/profile-placeholder.jpg';
-import profileImg from 'assets/profile.jpg';
+import profileImg from 'assets/profil.png';
 import { Button } from 'components/Button';
 import { DecoderText } from 'components/DecoderText';
 import { Divider } from 'components/Divider';
