cb3734202776001e2bd9efdbd7900ffb1b6a8528
Vation Code <abdullah@smainsankamil.sch.id>
2022-07-16 12:24:22 +0700

    test branch for next feature

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,11 +7,12 @@ export const Footer = ({ className }) => (
   <footer className={classes(styles.footer, className)}>
     <Text size="s" align="center">
       <span className={styles.date}>
-        {`© ${new Date().getFullYear()} Hamish Williams.`}
+        {`© ${new Date().getFullYear()} Hamish Williams.`} 
       </span>
       <Link secondary className={styles.link} href="/humans.txt" target="_self">
-        Crafted by yours truly
+        Crafted by Abdullah
       </Link>
     </Text>
   </footer>
 );
+// you can add attribution/copyright to this web on your own IF you have already modified this website and make the UI Looks significantly different with original design
--- src/components/Navbar/navData.js
+++ src/components/Navbar/navData.js
@@ -20,7 +20,7 @@ export const navLinks = [
 export const socialLinks = [
   {
     label: 'Twitter',
-    url: 'https://twitter.com/hamishMW',
+    url: 'https://twitter.com/nix',
     icon: 'twitter',
   },
   {
@@ -30,7 +30,7 @@ export const socialLinks = [
   },
   {
     label: 'Github',
-    url: 'https://github.com/HamishMW',
+    url: 'https://github.com/devdezzies',
     icon: 'github',
   },
 ];
