f8080f6533efb76ed6b39f68e63cb456695f0d5e
Vation Code <abdullah@smainsankamil.sch.id>
2022-07-17 10:54:00 +0700

    complete remove quote

--- src/pages/projects/readin/VolkiharKnight.js
+++ src/pages/projects/readin/VolkiharKnight.js
@@ -89,7 +89,7 @@ export function VolkiharKnight() {
                <ProjectTextRow>
               <ProjectSectionHeading>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</ProjectSectionHeading>
               <ProjectSectionText>
-              "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores 
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores 
               et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt 
               mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero 
               tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis 
