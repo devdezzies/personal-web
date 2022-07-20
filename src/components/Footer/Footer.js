726ecf040320111f2186febb24ff753173265d51
Vation Code <abdullah@smainsankamil.sch.id>
2022-07-21 06:59:09 +0700

    fix footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,23 +2,18 @@ import { Link } from 'components/Link';
 import { Text } from 'components/Text';
 import { classes } from 'utils/style';
 import styles from './Footer.module.css';
-import Script from 'next/script'
+import Script from 'next/script';
+import Head from 'next/head';
 
 export const Footer = ({ className }) => (
   <footer className={classes(styles.footer, className)}>
     <Text size="s" align="center">
-    <a
-        href="//24timezones.com/current_time/indonesia_bogor_clock.php"
-        className="clock24"
-        id="tz24-1658359276-c213124-eyJob3VydHlwZSI6IjEyIiwic2hvd2RhdGUiOiIxIiwic2hvd3NlY29uZHMiOiIxIiwic2hvd3RpbWV6b25lIjoiMSIsInR5cGUiOiJkIiwibGFuZyI6ImVuIn0="
-        title="Bogor timezone"
-        target="_blank"
-        rel="noreferrer"
-        
-      >
-        Bogor    
-      </a>
-      <Script type="text/javascript" src="//w.24timezones.com/l.js" async></Script>
+    <span className={styles.date}>
+        {`© ${new Date().getFullYear()} Hamish Williams.`} 
+      </span>
+      <Link secondary className={styles.link} href="/humans.txt" target="_self">
+        Crafted by Abdullah
+      </Link>
     </Text>
   </footer>
 );
